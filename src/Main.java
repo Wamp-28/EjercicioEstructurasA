a654820861a134d30188925dcffc97c1c5be9c3a
AndresSuarez <andress93774@gmail.com>
2025-02-19 14:25:36 -0500

    Mascota

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
         System.out.println("Hola profe");
 
+        System.out.println("Ivan Andres Suarez Arevalo");
+        System.out.println("Jugar Videojuegos");
+        System.out.println("Dibujar");
+        System.out.println("nombre de mascota:max");;
 
     }
 }
