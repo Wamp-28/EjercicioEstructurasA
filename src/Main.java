50f6770c6ace12e4f953eda65bb574f295632495
Winder <edi.corredor45@gmail.com>
2025-02-19 14:12:48 -0500

    datos personales winder

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
-
+        System.out.println("Winder Yair Gonzalez");
+        System.out.println("Escuchar Musica");
+        System.out.println("Futbol");
 
 
     }
