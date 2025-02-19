c9e6fabb19711577ecdec17213ecfbf7de5cda16
joan-ch-18 <joanchindoyh@gmail.com>
2025-02-19 14:54:43 -0500

    Datos Personales

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola Mundo");
-
-
-
-
-
-
+        System.out.println("Hola profe ya pude arreglar esta vaina");
+        System.out.println("Joan Belkham Chindoy Hernández");
+        System.out.println("Jugar videojuegos y entrenar");
+        System.out.println("Perra: Sasha");
 
 
 
