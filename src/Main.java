223009d297c23f16c51b2dd253e59f680fc43304
Kevinpisco <kevin.pisco@uniminuto.edu.co>
2025-02-19 14:27:42 -0500

    datos

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
-
-
-
-
-
-
-
-
-
+        System.out.println("Kevin Stiven Pisco Campos"); 
+        System.out.println("Ver videos");
+        System.out.println("jugar");
+        System.out.println("no tengo");
 
     }
 }
