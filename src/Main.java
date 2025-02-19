a6a3b8876bb2774c35e96ebeb59773ba01239f89
lina <sof.2081@gmail.com>
2025-02-19 14:20:54 -0500

    datos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
-        System.out.println("Hola profe");
+        System.out.println("Lina Sofia Villaraga Gomez");
+        System.out.println("Dibujar");
+        System.out.println("No tengo deporte favorito");
+        System.out.println("Gato:Copito");
 
 
     }
