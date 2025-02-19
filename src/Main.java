c68cfcf13680970a5e36fcda62c8ea3ccfb44303
KevinGo970577 <kengookvn@gmail.com>
2025-02-19 14:17:26 -0500

    Mascota

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
-
+        System.out.println("Kevin Santiago Gomez Castro");
+        System.out.println("escuchar Milo J");
+        System.out.println("Jugar voleibol");
+        System.out.println("Perro: Morita");
 
 
 
