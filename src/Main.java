602c3fc1123007950efc6d6567884940085d1a6d
Andres-Suarez-A <andress93774@gmail.com>
2025-02-19 14:26:25 -0500

    datos personales

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
+        System.out.println("Sebastian Arevalo Mgitatiz");
+        System.out.println("jugar  videojuegos");
+        System.out.println("futbol");
+        System.out.println("ninguna");
 
 
 
