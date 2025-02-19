bffdc9be58ff07e3fc3fc907ecf26ba5b42f7b35
maripachon <pachonmaritza102@gmail.com>
2025-02-19 14:21:24 -0500

    datos personales

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         System.out.println("Hola Mundo");
         System.out.println("Hola profe");
 
-
+        System.out.println("maritza pachon");
+        System.out.println("escuchar musica");
+        System.out.println("voleivol");
+        System.out.println("Gato: apolo");
     }
 }
