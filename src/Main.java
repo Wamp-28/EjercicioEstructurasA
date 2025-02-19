76709bff5dcda8688f44acbee6a694afc1d98733
Winder <edi.corredor45@gmail.com>
2025-02-19 14:14:26 -0500

    Mascota

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Winder Yair Gonzalez");
         System.out.println("Escuchar Musica");
         System.out.println("Futbol");
+        System.out.println("Perra : Princesa");
 
 
     }
