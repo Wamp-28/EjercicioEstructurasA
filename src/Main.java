f8d0ca845af5cae64a5fe21b63a5b036b03c26e0
joan-ch-18 <joanchindoyh@gmail.com>
2025-02-26 11:04:33 -0500

    Ejercicio switch case viejo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola profe ya pude arreglar esta vaina");
-        System.out.println("Joan Belkham Chindoy Hernández");
-        System.out.println("Jugar videojuegos y entrenar");
-        System.out.println("Perra: Sasha");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("    ------------------------------");
+        System.out.println("    ------------ MENÚ ------------");
+        System.out.println("    ------------------------------");
+        System.out.println("    1. Sumar dos número");
+        System.out.println("    2. Multiplicar dos números");
+        System.out.println("    Dígite la opción que desea elegir: ");
+
+        int opc = scanner.nextInt();
+        int n1, n2;
+
+        switch (opc){
+            case 1: System.out.println("    Dígite el primer número: ");
+            n1= scanner.nextInt();
+            System.out.println("    Dígite el segundo número: ");
+            n2= scanner.nextInt();
+
+            System.out.println("    La suma es: "+(n1+n2));
+            break;
+
+            case 2: System.out.println("    Dígite el primer número: ");
+                n1= scanner.nextInt();
+                System.out.println("    Dígite el segundo número: ");
+                n2= scanner.nextInt();
+
+                System.out.println("    La multiplicación es: "+(n1*n2));
+                break;
+            default: System.out.println("*** OPCIÓN NO VÁLIDA ***");
+        }
 
 
 
