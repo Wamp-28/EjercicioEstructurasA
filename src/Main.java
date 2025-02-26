faa93dae039369fef6bee3656e675ef0c5f95d70
Kevin Santos <kevinlich180@gmail.com>
2025-02-26 01:21:42 -0500

    Ejercicios switchCase

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,41 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.println("buenas");
-
-
-
-
-
-
-
-
+/*
+       System.out.println("buenas");
         System.out.println("Kevin Santiago Santos Castellanos");
         System.out.println("leer");
         System.out.println("ciclismo");
-        System.out.println("ninguna");
+        System.out.println("ninguna");*/
+        String Electrodomestico = "Lavadora";
 
+        switch (Electrodomestico.toLowerCase()) {
+            case "Nevera":
+                Electrodomestico = "Nevera";
+                break;
+            case "Televisor":
+                Electrodomestico = "Televisor";
+                break;
+            case "lavadora":
+                Electrodomestico = "Lavadora";
+                break;
+
+            default:
+                Electrodomestico = "No es tu electrodomestico";
+        }
+        System.out.println("Tu electrodomestico es\t" + Electrodomestico);
+
+        int OpBicicleta = 3;
+
+
+        String marcaBici =switch (OpBicicleta){
+            case 1 -> "Scott";
+            case 2 ->"Cannondate";
+            case 3 ->"Specialized";
+            default -> "No es tu bicicleta" ;
+
+        };
+        System.out.println("tu bicicleta es \t"+marcaBici);
     ;}
+
 }
